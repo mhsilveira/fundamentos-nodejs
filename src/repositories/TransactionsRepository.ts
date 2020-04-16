9acdfdcb90d4e9721fb8429fd36b7edac2a454fb
Matheus Silveira <blanchimaah@gmail.com>
2020-04-16 13:37:06 -0300

    Desafio Fundamentos NodeJS com TypeScript

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    /**
+     * Tive que ver no fórum essa parte, mais problemas com a sintaxe
+     * do que com a lógica
+     */
+    const balance = this.transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.value;
+        } else if (transaction.type === 'outcome') {
+          acc.outcome += transaction.value;
+        }
+        acc.total = acc.income - acc.outcome;
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const arrTransaction = {
+      title,
+      value,
+      type,
+    };
+    const transaction = new Transaction(arrTransaction);
+    // preciso colocar dentro de um indice isso aaa
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
